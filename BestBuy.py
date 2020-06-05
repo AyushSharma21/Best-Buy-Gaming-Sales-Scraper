721a79b14fc4b8e54f5559b7da5d695b70f6f74b
AyushSharma21 <47763441+AyushSharma21@users.noreply.github.com>
2020-06-05 11:57:39 -0400

    Update BestBuy.py

--- BestBuy.py
+++ BestBuy.py
@@ -1,16 +1,15 @@
-#Important import statements.
-import requests 
+import requests
 import json
 from dataclasses import dataclass
 
 @dataclass
-class Item: #This class represents an Item on the Sales Catalog. An Item has a name, regular price, regular price, and percent off. 
+class Item:
         name: str
         reg_price: float
         discount_price: float
         percent_off: float
 
-def partition(list, start, end): #Simple Partition Function, where we are sorting the passed in List by percent_off. 
+def partition(list, start, end):
     pivot = list[start].percent_off
     low = start + 1
     high = end
@@ -27,35 +26,27 @@ def partition(list, start, end): #Simple Partition Function, where we are sortin
     list[start], list[high] = list[high], list[start]
     return high
 
-def quick_sort(list, start, end): #With the partition function above, we can complete our quick-sort implmentation.
+def quick_sort(list, start, end):
     if start >= end:
         return
     p = partition(list, start, end)
     quick_sort(list, start, p-1)
     quick_sort(list, p+1, end)
-
-#response holds the first page of the Best Buy gaming sales catalog. 
-response = requests.get("https://www.bestbuy.ca/api/v2/json/sku-collections/17056?categoryid=&currentRegion=BC&include=facets%2C%20redirects&lang=en-CA&page=1&pageSize=24&path=&query=&exp=&sortBy=relevance&sortDir=desc") 
-#To make the page easier to work with, noticing it is in JSON format, I use the JSON library to store the page as a JSON collection that we can actually use. 
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
+response = requests.get("https://www.bestbuy.ca/api/v2/json/sku-collections/17056?categoryid=&currentRegion=BC&include=facets%2C%20redirects&lang=en-CA&page=1&pageSize=24&path=&query=&exp=&sortBy=relevance&sortDir=desc",headers=headers)
 json_collection = json.loads(response.text)
-#Collect constant variables, found through looking at the Page.  
 num_pages = int(json_collection['totalPages'])
 items_per_page = int(json_collection['pageSize'])
 total_items = int(json_collection['total'])
 item_list = []
 
 
-for page in range(1, num_pages+1): #We loop starting at page 1, to the last page. 
-    #To get the unique URL for each page, I use an f String which will update the url corresponding to the page variable.  
+for page in range(1, num_pages+1):
     url = f'https://www.bestbuy.ca/api/v2/json/sku-collections/17056?categoryid=&currentRegion=BC&include=facets%2C%20redirects&lang=en-CA&page={page}&pageSize=24&path=&query=&exp=&sortBy=relevance&sortDir=desc'
-    #Format current Page to JSON. 
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
     json_data = json.loads(response.text)
-    #If the scraper is at the last page, 
     if(page == num_pages):
-    #The last page may contain less items than the items_per_page, and so to account for that, we manually calculate the number of items on said page. 
         for i in range(1, total_items - ((num_pages-1) * items_per_page) + 1):
-            #collect data, apply to type Item, and append to the list.  
             name = json_data['products'][i-1]['name']
             reg_price = float(json_data['products'][i-1]['regularPrice'])
             discount_price = float(json_data['products'][i-1]['salePrice'])
@@ -63,15 +54,13 @@ for page in range(1, num_pages+1): #We loop starting at page 1, to the last page
             item_list.append(Item(name, reg_price, discount_price, percent_off))
     else:
         for i in range(1, items_per_page+1):
-            #collect data, apply to type Item, and append to the list. 
             name = json_data['products'][i-1]['name']
             reg_price = float(json_data['products'][i-1]['regularPrice'])
             discount_price = float(json_data['products'][i-1]['salePrice'])
             percent_off = 1 - discount_price / reg_price
             item_list.append(Item(name, reg_price, discount_price, percent_off))
-         
-#Sort List, and print to terminal.  
-quick_sort(item_list, 0, len(item_list) - 1) 
+
+quick_sort(item_list, 0, len(item_list) - 1)
 for i in range(0, len(item_list)):
     print("Product Name:", item_list[i].name)
     print("Regular Price:", '${:,.2f}'.format(item_list[i].reg_price), "Discounted Price:", '${:,.2f}'.format(item_list[i].discount_price), "Percent off:", str(int(100 * item_list[i].percent_off))+'%')
